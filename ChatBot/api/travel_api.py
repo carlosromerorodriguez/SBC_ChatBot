09cf36ef7c6e284fde05bdc33ad0fb33550da372
carlosromerorodriguez <c.romero@students.salle.url.edu>
2024-05-28 17:12:13 +0200

    Initial Project Structure

--- ChatBot/api/travel_api.py
+++ ChatBot/api/travel_api.py
@@ -1,4 +1,8 @@
 class TravelAPI:
-    def get_info(self, intent, entities):
+    def get_hotel_info(self, intent, entities):
         # Lógica para llamar a la API de Booking.com
+        pass
+
+    def get_flight_info(self, intent, entities):
+
         pass
